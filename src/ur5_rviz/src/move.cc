49111ee21bf7c29e49e946b1ec1ec90a951dc1df
moongerim <aigerim.nurbayeva@nu.edu.kz>
2022-04-20 14:29:34 +0600

    part6

---
+++ src/ur5_rviz/src/move.cc
@@ -0,0 +1,86 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include "std_msgs/Float64MultiArray.h"
+#include <cmath>
+
+double ctp[21]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+void spheres_callback(const std_msgs::Float64MultiArray msg) 
+{
+    for (int i=0; i<21; i++) {
+        ctp[i] = msg.data[i];
+    }
+}
+
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "points_and_lines");
+  ros::NodeHandle n;
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+  ros::Subscriber points = n.subscribe("/LowController/spheres", 1, spheres_callback);
+  ros::Rate loop_rate(40);
+
+//   float f = 0.0;
+  while (ros::ok())
+  {
+
+    visualization_msgs::Marker points, line_strip, line_list;
+    points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/base_link";
+    points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
+    points.ns = line_strip.ns = line_list.ns = "points_and_lines";
+    points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
+    points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
+    points.id = 0;
+    line_strip.id = 1;
+    line_list.id = 2;
+    points.type = visualization_msgs::Marker::POINTS;
+    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+    line_list.type = visualization_msgs::Marker::LINE_LIST;
+
+    // POINTS markers use x and y scale for width/height respectively
+    points.scale.x = 0.02;
+    points.scale.y = 0.02;
+
+    // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
+    line_strip.scale.x = 0.1;
+    line_list.scale.x = 0.1;
+
+    // Points are green
+    points.color.g = 1.0f;
+    points.color.a = 1.0;
+
+    // Line strip is blue
+    line_strip.color.b = 1.0;
+    line_strip.color.a = 1.0;
+
+    // Line list is red
+    line_list.color.r = 1.0;
+    line_list.color.a = 1.0;
+
+    // Create the vertices for the points and lines
+    for (int i = 0; i < 7; i++)
+    {
+      geometry_msgs::Point p;
+      p.x = ctp[i*3];
+      p.y = ctp[i*3+1];
+      p.z = ctp[i*3+2];
+
+      points.points.push_back(p);
+      line_strip.points.push_back(p);
+
+      // The line list needs two points for each line
+      line_list.points.push_back(p);
+    //   p.z += 1.0;
+      line_list.points.push_back(p);
+    }
+
+    marker_pub.publish(points);
+    marker_pub.publish(line_strip);
+    marker_pub.publish(line_list);
+
+    ros::spinOnce();
+  }
+  ros::spin();
+  return 0;
+}
